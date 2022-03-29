7b830760505e89d11e761f41eaee4243a632b8df
Jonathan BODET <jonathan.bodet@conserto.pro>
2022-03-29 13:06:00 +0200

    navogator

---
+++ src/components/Navigator.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+
+export class Navigator extends React.Component {
+    constructor(props) {
+        super(props);
+      }
+    render() {
+      return <div className='navigator-ctn'>
+          Navigator
+      </div>;
+    }
+  }
